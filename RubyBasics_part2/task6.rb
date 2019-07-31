d9f28628963b78afdea89efb293ebba9c6cc9247
Vadim Savostin <vsavdev@gmail.com>
2019-07-31 15:27:50 +0300

    some refactoring

--- RubyBasics_part2/task6.rb
+++ RubyBasics_part2/task6.rb
@@ -11,42 +11,43 @@ cart = {}
 cart_total = 0
 
 loop do
-  puts "Введите название товара (стоп для завершения): "
+  puts 'Введите название товара (стоп для завершения): '
   item_name = gets.strip.capitalize
-  break if item_name == "Стоп"
-  until item_name != ""
-    puts "Наименование не может быть пустым: "
+  break if item_name == 'Стоп'
+
+  while item_name.empty?
+    puts 'Наименование не может быть пустым: '
     item_name = gets.strip.capitalize
   end
-    puts "Введите цену за единицу товара: "
-    item_price = gets.chomp.to_f
-  until item_price > 0
-    puts "Введите положительное число: "
+
+  puts 'Введите цену за единицу товара: '
+  item_price = gets.chomp.to_f
+  until item_price.positive?
+    puts 'Введите положительное число: '
     item_price = gets.chomp.to_f
   end
-    puts "Введите количество купленного товара: "
-    item_count = gets.chomp.to_f
-  until item_count > 0
-    puts "Введите положительное число: "
+
+  puts 'Введите количество купленного товара: '
+  item_count = gets.chomp.to_f
+  until item_count.positive?
+    puts 'Введите положительное число: '
     item_count = gets.chomp.to_f
   end
-    cart[item_name] = {item_price => item_count} 
+
+  cart[item_name] = { item_price => item_count }
 end
 
-system "clear"
+system 'clear'
 
 cart.each do |key, value|
   puts "Наименование товара: #{key}"
-    value.each do |price, count|
-      puts "Цена за единицу товара: #{price}"
-      puts "Количество: #{count}"
-      item_total = price * count
-      puts  "Сумма: #{item_total}"
-      cart_total += item_total
-    end  
+  value.each do |price, count|
+    puts "Цена за единицу товара: #{price}"
+    puts "Количество: #{count}"
+    item_total = price * count
+    puts "Сумма: #{item_total}"
+    cart_total += item_total
+  end
 end
-puts "*"*40
+puts '*' * 40
 puts "Общая сумма за все товары: #{cart_total}"
-
-
-
