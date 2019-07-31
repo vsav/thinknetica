14be7de4823d0c1ed47e1e1f8c8a1eb0fd40fc07
Vadim Savostin <vsavdev@gmail.com>
2019-07-31 15:26:16 +0300

    some refactoring

--- RubyBasics_part2/task1.rb
+++ RubyBasics_part2/task1.rb
@@ -1,11 +1,8 @@
 #  1. Сделать хеш, содеращий месяцы и количество дней в месяце. В цикле выводить те месяцы, у которых количество дней ровно 30
 
-months = {january: 31, february: 28, march: 31, april: 30, may: 31, june: 30, july: 31, august: 31, september: 30, october: 31, november: 30, december: 31}
+months = { january: 31, february: 28, march: 31, april: 30, may: 31, june: 30,
+           july: 31, august: 31, september: 30, october: 31, november: 30,
+           december: 31 }
 
-puts "Месяцы, у которых количество дней равно 30:"
-months.each do |key, value|
-  if value == 30
-    puts key
-  end
-end
-
+puts 'Месяцы, у которых количество дней равно 30:'
+months.each { |key, value| puts key if value == 30 }
