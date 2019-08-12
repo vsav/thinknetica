fabf29152ca90f7eb4c4364c53977e8d7123b20b
Vadim Savostin <vsavdev@gmail.com>
2019-08-12 23:28:54 +0300

    carriage file split

--- Blocks/main.rb
+++ Blocks/main.rb
@@ -7,6 +7,8 @@ require_relative './station.rb'
 require_relative './route.rb'
 require_relative './cargo_train.rb'
 require_relative './carriage.rb'
+require_relative './pass_carriage.rb'
+require_relative './cargo_carriage.rb'
 require_relative './passenger_train.rb'
 
 rr = RailRoad.new
